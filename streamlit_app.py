f1394351ebd06c0271d6f59eca554576f0be45e8
Martín Badino <151761027+MartoBadi@users.noreply.github.com>
2024-09-06 18:15:38 -0300

    Add files via upload
    Se cambió el orden de la clase cartera por el de pulover en la lista de clases del archivo streamlit_app.py volviendo a estar las clases en el orden original (que por cierto me di cuenta al leer el archivo .ipynb que ese es el orden con el que están las imágenes en el dataset).

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ model = load_model('fashion_CNN_new.h5')
 
 # Define class labels for Fashion MNIST dataset
 class_labels = [
-    'remera', 'pantalón', 'cartera', 'vestido', 'sobretodo', 'sandalia', 'camisa', 'zapatilla', 'pulóver', 'bota'
+    'remera', 'pantalón', 'pulóver', 'vestido', 'sobretodo', 'sandalia', 'camisa', 'zapatilla', 'cartera', 'bota'
 ]
 
 # Function to preprocess uploaded image
@@ -24,7 +24,7 @@ def main():
     st.markdown("""
     Esta aplicación web ha sido creada por Martín Badino para la materia Modelizado de Sistemas de IA de la carrera de Ciencias de Datos del IFTS 18. Esta aplicación web tiene integrado un modelo de IA que usa una red neuronal artificial para clasificar imágenes de prendas de ropa en las siguientes categorías: remera, pantalón, cartera, vestido, sobretodo, sandalia, camisa, zapatilla, pulóver y bota.
 
-    La idea es que subas una imagen de una prenda de ropa que pertenezca a las categorías mencionadas y la aplicación te dirá a qué categoría pertenece (scrollear hacia abajo hasta donde dice: drag and drop file here).
+    La idea es que subas una imagen jpg, jpeg o png de una prenda de ropa que pertenezca a las categorías mencionadas y la aplicación te dirá a qué categoría pertenece (si estás viendo esta página desde un celular scrolleá hacia abajo hasta donde dice: browse files). Puedes sacarle una foto a tu prenda o buscar una imagen en Pinterest (ahí podés encontrar fácilmente fotos de prendas de ropa solas con un fondo blanco, o sea sin que en las fotos haya un modelo con la ropa puesta).
     """)
     
     uploaded_file = st.file_uploader("Adjuntá una imagen jpg, jpeg o png.", type=["jpg", "jpeg", "png"])
